2373742cf1a5d6e04db5dea12f37a523ba0e1be7
Jeremy Tucker <jeremytucker76@gmail.com>
2022-10-14 08:22:25 -0400

    still needs work

---
+++ PalindromeExercise/Palindrome.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xunit;
+
+namespace Palindrome.Test
+{
+    public class UnitTest1
+    {
+        [Theory]
+        [InlineData()]
+        public void Test1()
+        {
+
+        }
+    }
+}
+
